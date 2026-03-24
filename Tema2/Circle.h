2b4af074fed452546c6994c5e6cab159edea9cce
NatyStoik <161611376+NatyStoik@users.noreply.github.com>
2026-03-24 15:27:04 +0200

    Add Circle.h with circle rasterization functions

---
+++ Tema2/Circle.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void RasterizeCircleMidpoint(int cx, int cy, int radius);
+void RasterizeCircleMidpointFilled(int cx, int cy, int radius);
+
+void DrawCircleDemoOutline();
+void DrawCircleDemoFilled();
