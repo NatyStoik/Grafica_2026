8a357ff08ca9753ef93f2d7fdd9a6fe9b357a88e
NatyStoik <161611376+NatyStoik@users.noreply.github.com>
2026-03-24 15:27:46 +0200

    Add Line.h header with line rasterization functions

---
+++ Tema2/Line.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void RasterizeLineMidpoint(int x0, int y0, int x1, int y1);
+void RasterizeLineMidpointThick(int x0, int y0, int x1, int y1, int thickness);
+
+void DrawLineDemoAllOctants();
+void DrawLineDemoPolygon();
+void DrawLineDemoPolygonThick();
